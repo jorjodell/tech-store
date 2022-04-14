6d97c0d0fd782c60ee0be4e249d9efe6f89e1d91
Adylbek Dzhoroev <adikjoro@gmail.com>
2022-04-14 18:55:17 +0600

    Start creating a header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,54 @@
+import { ReactComponent as Logo } from '../assets/logo.svg';
+import { ReactComponent as SearchIcon } from '../assets/search.svg';
+
 function Header() {
   return (
-    <header>
-      <h1>Tech store</h1>
+    <header className="header">
+      <div className="header-top">
+        <div className="container header-top__content">
+          <div className="header-top__schedule">Mon-Thu: 9:00 AM - 5:30 PM</div>
+          <p className="header-top__address">
+            Visit our showroom in 1234 Street Adress City Address, 1234{' '}
+            <a href="#">Contact Us</a>
+          </p>
+          <div className="header-top__contacts">
+            <div className="header-top__phone">
+              Call Us: <a href="tel:0012345678">(00) 1234 5678</a>
+            </div>
+            <a href="https://facebook.com">
+              <img src="" alt="F" />
+            </a>
+            <a href="https://instagram.com">
+              <img src="" alt="I" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="container header-main">
+        <Logo />
+        <nav className="links">
+          <a href="#">Laptops</a>
+          <a href="#">Desktop PCs</a>
+          <a href="#">Networking Devices</a>
+          <a href="#">PC Parts</a>
+          <a href="#">All Other Products</a>
+          <a href="#">Repairs</a>
+          <a href="#">Our Deals</a>
+        </nav>
+        <div className="actions">
+          <button className="btn">
+            <SearchIcon />
+          </button>
+          <button className="btn">
+            <img src="" alt="" />
+          </button>
+          <button className="avatar">
+            <img src="" alt="" />
+          </button>
+        </div>
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;
